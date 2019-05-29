9c19ccabd3fd3055e8250214c7c495f03ffc25c7
Jared Cole-Lewis <jaredcole-lewis@Jareds-Air.fios-router.home>
2019-05-29 10:11:27 -0400

    creating jumbotron.

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./jumbotron.css";
+
+const Jumbotron = () => (
+	<header className = "header">
+		<h1>Cartoon Clicky Game!</h1>
+		<h2>Match all of the pictures and your a winner.</h2>
+	</header>
+);
+export default Jumbotron;
